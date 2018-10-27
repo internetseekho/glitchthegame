eec639be070e7f790678623abec91d85f34bcbfd
root <mjks4545@gmail.com>
2018-10-27 08:04:04 -0700

    Game initiated
    All of the code and assests you can get till video number 4

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadLevelTime;
+
+    public void Start()
+    {
+        Invoke( "LoadNextLevel", autoLoadLevelTime);    
+    }
+
+    public void LoadLevel(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        print("Quit request received");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log( "Name of object is " + name);
+    }
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        Debug.Log( levelMusicChangeArray[level] );
+        if (levelMusicChangeArray[level]) {
+            audioSource.clip = levelMusicChangeArray[level];
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
